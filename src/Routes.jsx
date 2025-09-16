8fe74fdcef686d2d58f1d9166ed71b08a2578fb4
undefined <etimpaul22@gmail.com>
2025-09-17 00:55:09 +0100

    project setup

---
+++ src/Routes.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "components/ScrollToTop";
+import ErrorBoundary from "components/ErrorBoundary";
+import NotFound from "pages/NotFound";
+import ContractInputDashboard from './pages/contract-input-dashboard';
+import SDKConfiguration from './pages/sdk-configuration';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your route here */}
+        <Route path="/" element={<ContractInputDashboard />} />
+        <Route path="/sdk-configuration" element={<SDKConfiguration />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
